7268c86ba6e65d754767487b767b73e0d085a12a
Dominic Bett <dbett49@gmail.com>
2023-08-29 07:22:45 +0300

    add more features

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,6 +4,12 @@ import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -19,5 +25,14 @@ export class TaskService {
   deleteTask(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
+  }
+
+  addTask(task: Task): Observable<Task> {
+    return this.http.post<Task>(this.apiUrl, task, httpOptions);
   }
 }
